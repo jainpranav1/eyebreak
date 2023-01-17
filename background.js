29321de139932b2f09ddc2bcdf417a6623e13a7a
jainpranav1 <pranavji@tamu.edu>
2023-01-16 20:19:52 -0600

    timer works now fully

--- background.js
+++ background.js
@@ -1,3 +1,96 @@
-console.log("background!")
+let work_seconds = null;
+let break_seconds = null;
+let work_interval = null;
+let break_interval = null;
+let main_port = null;
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
+function format_time(seconds) {
+
+    let secs = seconds % 60;
+    let mins = Math.floor(seconds / 60);
+
+    if (secs < 10) {
+        secs = `0${secs}`;
+    }
+
+    if (mins < 10) {
+        mins = `0${mins}`;
+    }
+
+    return `${mins}:${secs}`;
+}
+
+function work_timer() {
+    work_seconds = 20 * 60;
+
+    if (main_port) {
+        main_port.postMessage({ time: format_time(work_seconds) })
+    }
+
+    work_interval = setInterval(() => {
+
+        if (work_seconds == 1) {
+            work_seconds = null;
+            clearInterval(work_interval);
+            break_timer();
+            return;
+        }
+
+        work_seconds--;
+
+        if (main_port) {
+            main_port.postMessage({ time: format_time(work_seconds) })
+        }
+
+    }, 1000);
+}
+
+function break_timer() {
+    break_seconds = 20;
+
+    if (main_port) {
+        main_port.postMessage({ time: format_time(break_seconds) })
+    }
+
+    break_interval = setInterval(() => {
+
+        if (break_seconds == 1) {
+            break_seconds = null;
+            clearInterval(break_interval);
+            work_timer();
+            return;
+        }
+
+        break_seconds--;
+
+        if (main_port) {
+            main_port.postMessage({ time: format_time(break_seconds) })
+        }
+
+    }, 1000);
+}
+
+chrome.runtime.onConnect.addListener(port => {
+    main_port = port;
+
+    if (work_seconds) {
+        port.postMessage({ time: format_time(work_seconds) })
+    }
+    if (break_seconds) {
+        port.postMessage({ time: format_time(break_seconds) })
+    }
+
+    port.onMessage.addListener(msg => {
+        if (msg.button_pressed == "on") {
+            work_timer();
+        }
+        else if (msg.button_pressed == "off") {
+            clearInterval(work_interval);
+            clearInterval(break_interval);
+            port.postMessage({ time: format_time(20 * 60) })
+        }
+    });
+    port.onDisconnect.addListener(() => {
+        main_port = null;
+    });
+});
--- popup.js
+++ popup.js
@@ -1,71 +1,18 @@
-// global variables
-const time_el = document.querySelector('.time');
-const on_btn = document.getElementById('on');
-const off_btn = document.getElementById('off');
-let work_seconds = null;
-let break_seconds = null;
-let work_interval = null;
-let break_interval = null;
+let time_el = document.querySelector('.time');
+let on_btn = document.getElementById('on');
+let off_btn = document.getElementById('off');
+let port = chrome.runtime.connect({ name: "popup-background communication" });
 
-function format_time(seconds) {
-
-    let secs = seconds % 60;
-    let mins = Math.floor(seconds / 60);
-
-    if (secs < 10) {
-        secs = `0${secs}`;
+port.onMessage.addListener(msg => {
+    if (msg.time) {
+        time_el.innerHTML = msg.time;
     }
+});
 
-    if (mins < 10) {
-        mins = `0${mins}`;
-    }
+on_btn.addEventListener("click", () => {
+    port.postMessage({ button_pressed: "on" })
+});
 
-    return `${mins}:${secs}`;
-}
-
-function work_timer() {
-    work_seconds = 20 * 60;
-    time_el.innerHTML = format_time(work_seconds);
-
-    work_interval = setInterval(() => {
-
-        if (work_seconds == 0) {
-            clearInterval(work_interval);
-            break_timer();
-            return;
-        }
-
-        work_seconds--;
-        time_el.innerHTML = format_time(work_seconds);
-    }, 1000);
-}
-
-function break_timer() {
-    break_seconds = 20;
-    time_el.innerHTML = format_time(break_seconds);
-
-    break_interval = setInterval(() => {
-
-        if (break_seconds == 0) {
-            clearInterval(break_interval);
-            work_timer();
-            return;
-        }
-
-        break_seconds--;
-        time_el.innerHTML = format_time(break_seconds);
-    }, 1000);
-}
-
-function on() {
-    work_timer();
-}
-
-function off() {
-    clearInterval(work_interval);
-    clearInterval(break_interval);
-    time_el.innerHTML = '20:00';
-}
-
-on_btn.addEventListener("click", on);
-off_btn.addEventListener("click", off);
+off_btn.addEventListener("click", () => {
+    port.postMessage({ button_pressed: "off" })
+});
