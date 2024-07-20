56f0dd5b62a41d0d075e2a209e4a87894f92b66f
Pranav Jain <the.pranav.jain.1@gmail.com>
2024-07-20 09:56:26 -0500

    replace callbacks with async await code

--- background.js
+++ background.js
@@ -2,68 +2,69 @@ let WORK_TIME_SEC = 20; // 20 * 60;
 let BREAK_TIME_SEC = 20;
 let VIDEO_URL = "https://youtu.be/MKC9LvRivTM?t=7";
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.storage.local.set({
     is_on: false,
     tab_id: null,
     alarm_suffix: Date.now(),
   });
 });
 
-chrome.runtime.onStartup.addListener(() => {
-  chrome.storage.local.set({
+chrome.runtime.onStartup.addListener(async () => {
+  await chrome.storage.local.set({
     is_on: false,
     tab_id: null,
     alarm_suffix: Date.now(),
   });
 });
 
-chrome.action.onClicked.addListener(() => {
-  chrome.storage.local.get(["is_on", "alarm_suffix"], (result) => {
-    if (result.is_on) {
-      chrome.storage.local.set({ is_on: false, tab_id: null });
-      chrome.action.setIcon({ path: "images/red_eyeball.png" });
-      chrome.alarms.clearAll();
-    } else {
-      chrome.storage.local.set({ is_on: true });
-      chrome.action.setIcon({ path: "images/green_eyeball.png" });
-      chrome.alarms.create("work alarm" + result.alarm_suffix, {
-        delayInMinutes: (WORK_TIME_SEC * 1) / 60,
-      });
-    }
-  });
+chrome.action.onClicked.addListener(async () => {
+  let result = await chrome.storage.local.get(["is_on", "alarm_suffix"]);
+
+  if (result.is_on) {
+    await chrome.storage.local.set({ is_on: false, tab_id: null });
+    await chrome.action.setIcon({ path: "images/red_eyeball.png" });
+    await chrome.alarms.clearAll();
+  } else {
+    await chrome.storage.local.set({ is_on: true });
+    await chrome.action.setIcon({ path: "images/green_eyeball.png" });
+    await chrome.alarms.create("work alarm" + result.alarm_suffix, {
+      delayInMinutes: (WORK_TIME_SEC * 1) / 60,
+    });
+  }
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.storage.local.get(["tab_id", "alarm_suffix"], (result) => {
-    if (alarm.name == "work alarm" + result.alarm_suffix) {
-      chrome.alarms.clear("work alarm" + result.alarm_suffix);
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  let result = await chrome.storage.local.get(["tab_id", "alarm_suffix"]);
 
-      chrome.tabs.create({ url: VIDEO_URL }, (tab) => {
-        chrome.storage.local.set({ tab_id: tab.id });
-        chrome.alarms.create("break alarm" + result.alarm_suffix, {
-          delayInMinutes: (BREAK_TIME_SEC * 1) / 60,
-        });
-      });
-    } else if (alarm.name == "break alarm" + result.alarm_suffix) {
-      chrome.alarms.clear("break alarm" + result.alarm_suffix);
-      if (result.tab_id != null) {
-        chrome.tabs.remove(result.tab_id);
-      }
-      chrome.storage.local.set({ tab_id: null });
-      chrome.alarms.create("work alarm" + result.alarm_suffix, {
-        delayInMinutes: (WORK_TIME_SEC * 1) / 60,
-      });
-    } else {
-      chrome.alarms.clear(alarm.name);
+  if (alarm.name == "work alarm" + result.alarm_suffix) {
+    await chrome.alarms.clear("work alarm" + result.alarm_suffix);
+
+    let tab = await chrome.tabs.create({ url: VIDEO_URL });
+    await chrome.storage.local.set({ tab_id: tab.id });
+
+    await chrome.alarms.create("break alarm" + result.alarm_suffix, {
+      delayInMinutes: (BREAK_TIME_SEC * 1) / 60,
+    });
+  } else if (alarm.name == "break alarm" + result.alarm_suffix) {
+    await chrome.alarms.clear("break alarm" + result.alarm_suffix);
+
+    if (result.tab_id != null) {
+      await chrome.tabs.remove(result.tab_id);
     }
-  });
+    await chrome.storage.local.set({ tab_id: null });
+    await chrome.alarms.create("work alarm" + result.alarm_suffix, {
+      delayInMinutes: (WORK_TIME_SEC * 1) / 60,
+    });
+  } else {
+    await chrome.alarms.clear(alarm.name);
+  }
 });
 
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  chrome.storage.local.get(["tab_id"], (result) => {
-    if (tabId == result.tab_id) {
-      chrome.storage.local.set({ tab_id: null });
-    }
-  });
+chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
+  let result = await chrome.storage.local.get(["tab_id"]);
+
+  if (tabId == result.tab_id) {
+    await chrome.storage.local.set({ tab_id: null });
+  }
 });
